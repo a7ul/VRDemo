2199ade57f9739a112bccd608ed315f816554edd
Atul <atulanand94@gmail.com>
2016-06-13 22:10:48 +0530

    Fix

--- js/index.js
+++ js/index.js
@@ -1,9 +1,25 @@
-$('#house-1').click(()=>{
-  console.log('clicked1');
+$('#house-1').click(() => {
+  console.log('house1-clicked');
+  $('#menu').attr('visible', false);
+  $('#photosphere-house').attr('visible', true);
+  $('#sky').attr('src', '#house-bg');
 });
-$('#house-2').click(()=>{
+
+$('#photosphere-back-button').click(() => {
+  console.log('photosphere-back-button-clicked');
+  $('#menu').attr('visible', true);
+  $('#photosphere-house').attr('visible', false);
+  $('#sky').attr('src', '#menu-bg');
+});
+
+$('#house-2').click(() => {
   console.log('clicked2');
 });
-$('#house-3').click(()=>{
+$('#house-3').click(() => {
   console.log('clicked3');
 });
+$('#photosphere-back-button').click(() => {
+  console.log('clicked back');
+});
+
+$('#sky').attr('src', '#menu-bg');
