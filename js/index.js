5ce7f8770c5eb845440a30bd25e074f1fd2984e1
Atul <atulanand94@gmail.com>
2016-06-14 06:21:32 +0530

    Fix

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ $('#house-1').click(() => {
   console.log('house1-clicked');
   $('#menu').attr('visible', false);
   $('#photosphere-house').attr('visible', true);
+  $('#3d-house').attr('visible', false);
   $('#sky').attr('src', '#house-bg');
 });
 
@@ -9,17 +10,30 @@ $('#photosphere-back-button').click(() => {
   console.log('photosphere-back-button-clicked');
   $('#menu').attr('visible', true);
   $('#photosphere-house').attr('visible', false);
+  $('#3d-house').attr('visible', false);
+  $('#sky').attr('src', '#menu-bg');
+});
+
+$('#3d-house-back-button').click(() => {
+  console.log('3d-house-back-button-clicked');
+  $('#menu').attr('visible', true);
+  $('#photosphere-house').attr('visible', false);
+  $('#3d-house').attr('visible', false);
   $('#sky').attr('src', '#menu-bg');
 });
 
 $('#house-2').click(() => {
   console.log('clicked2');
 });
+
 $('#house-3').click(() => {
-  console.log('clicked3');
-});
-$('#photosphere-back-button').click(() => {
-  console.log('clicked back');
+  console.log('house3-clicked');
+  $('#menu').attr('visible', false);
+  $('#photosphere-house').attr('visible', false);
+  $('#3d-house').attr('visible', true);
+  // $('#sky').attr('src', '#3d-bg');
 });
 
+$('#photosphere-house').attr('visible',false);
+$('#3d-house').attr('visible',false);
 $('#sky').attr('src', '#menu-bg');
